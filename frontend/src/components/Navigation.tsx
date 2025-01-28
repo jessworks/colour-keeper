fbce49a4cb9b2483eeff978f034ed26f2974e2ad
Jessika Ljungberg <jessika.ljungberg@gmail.com>
2025-01-28 13:12:41 +0100

    feat: burger menu

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-export const Navigation = () => {
-  return (
-    <nav className='navbar'>
 
-      <ul className='nav'>
+export const Navigation = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const toggleMenu = () => {
+    setIsActive(!isActive);
+  };
+
+  return (
+    <nav className={`navbar ${isActive ? 'active' : ''}`}>
+       <button className={`burger-menu ${isActive ? 'active' : ''}`} onClick={toggleMenu} aria-label="Toggle Menu">
+        <span className={isActive ? 'bar active' : 'bar'}></span>
+        <span className={isActive ? 'bar active' : 'bar'}></span>
+        <span className={isActive ? 'bar active' : 'bar'}></span>
+      </button>
+
+      <ul className="nav">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Login">Login</Link></li>
         <li><Link to="/Colors">Colours</Link></li>
