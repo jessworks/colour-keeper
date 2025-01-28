46880f1ca77d730d20d8bbfe986681010dcc3bfe
Jessika Ljungberg <jessika.ljungberg@gmail.com>
2025-01-28 10:28:01 +0100

    feat: background-colors set

--- frontend/src/components/Navigation.tsx
+++ frontend/src/components/Navigation.tsx
@@ -1,10 +1,10 @@
-// Navigation.tsx
 import { Link } from 'react-router-dom';
 
 export const Navigation = () => {
   return (
-    <nav>
-      <ul>
+    <nav className='navbar'>
+
+      <ul className='nav'>
         <li><Link to="/">Home</Link></li>
         <li><Link to="/Login">Login</Link></li>
         <li><Link to="/Colors">Colours</Link></li>
