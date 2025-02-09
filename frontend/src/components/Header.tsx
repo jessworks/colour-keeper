890d06be453a29bfca3f7deeed0c49b6b0c416f8
Jessika Ljungberg <jessika.ljungberg@gmail.com>
2025-02-09 14:45:55 +0100

    feat: header visibility at scroll

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,14 +1,32 @@
-import { Navigation } from "./Navigation";
+import { useState, useEffect, useRef } from 'react';
+import { Navigation } from './Navigation';
+import '../styles/components/header.scss';
+
 
 export const Header = () => {
-    return (
-        <>
-            <header>
-                <h1>Colour Keeper</h1>
+  const [isVisible, setIsVisible] = useState(true);
+  const lastScrollTop = useRef(0); // Use useRef to store the last scroll position
 
-                <Navigation />
-            </header>
-            
-        </>
-    );
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScroll = window.pageYOffset;
+
+      if (currentScroll > lastScrollTop.current) {
+        setIsVisible(false);
+      } else {
+        setIsVisible(true);
+      }
+      lastScrollTop.current = currentScroll <= 0 ? 0 : currentScroll; // Update the scroll position
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []); // Empty dependency array ensures this effect runs only once
+
+  return (
+    <header className={`header ${isVisible ? 'visible' : 'hidden'}`}>
+      <h1>Colour Keeper</h1>
+      <Navigation />
+    </header>
+  );
 };
